9fc1cccffef3222333867c569ca998cd71de8db1
MonolinkTV <monolinktv@gmail.com>
2019-06-28 22:39:02 -0400

    Started the main application

--- MainWindow.py
+++ MainWindow.py
@@ -1,18 +1,39 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtWidgets import QApplication
+import sys
+
+CSSSTARTER = "background-color: rgb"
+mainLayout = QVBoxLayout()
+
+if sys.platform == "win32":
+    import ctypes
+    FLID = u"flightlogger.0.0.1"  # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(FLID)
 
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.show()
+        self.setLayout(mainLayout)
+        self.setStyleSheet(CSSSTARTER + "(33, 33, 33)")
+        self.showMaximized()
+        self.setWindowTitle("Flight Logger")
+        self.icon = QIcon("icon.png")
+        self.setWindowIcon(self.icon)
+        newEntryBtn = QPushButton()
+        newEntryBtn.setGeometry(200, 200, 50, 25)
+        newEntryBtn.setText("New Entry")
+        newEntryBtn.setStyleSheet(CSSSTARTER + "(255, 255, 255)")
 
+
+mainApplication = QApplication([])
 
 mainWindow = MainWindow()
-mainWindow.show()
-mainApplication = QApplication([])
+mainApplication.setWindowIcon(mainWindow.icon)
+
 mainApplication.exec_()
 
 
