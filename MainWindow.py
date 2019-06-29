d88bb6f4167bace603077529558f78eeac1db950
MonolinkTV <monolinktv@gmail.com>
2019-06-28 20:25:07 -0400

    Start if the application

---
+++ MainWindow.py
@@ -0,0 +1,16 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+
+mainApplication = QApplication()
+
+
+
